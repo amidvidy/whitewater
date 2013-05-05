5bdbf5abcfc6317717318757eec2784e0aff28f6
Adam Midvidy <amidvidy@gmail.com>
2013-05-05 14:12:29 -0700

    normal 2-space tabs. sorry karoun

--- src/serverstate.rb
+++ src/serverstate.rb
@@ -2,38 +2,38 @@ require 'rubygems'
 require 'bud'
 
 module ServerStateProto
-	state	do
-		interface input, :update_term, [:term]
-		interface input, :update_max_term_voted, [:max_term]
-		interface input, :update_role, [:role]
+  state do
+    interface input, :update_term, [:term]
+    interface input, :update_max_term_voted, [:max_term]
+    interface input, :update_role, [:role]
 
-		interface output, :current_term, [:term]
-		interface output, :max_term_voted, [:max_term]
-		interface output, :current_role, [:role]
-		interface output, :current_members, [:host]
-	end
+    interface output, :current_term, [:term]
+    interface output, :max_term_voted, [:max_term]
+    interface output, :current_role, [:role]
+    interface output, :current_members, [:host]
+  end
 end
 
 module ServerStateImpl
-	include ServerStateProto
+  include ServerStateProto
 
-	state do
-		lmax :term
-		lmax :term_voted
-		table :members, [:host]
-		table :role, [] => [:role]
-	end
+  state do
+    lmax :term
+    lmax :term_voted
+    table :members, [:host]
+    table :role, [] => [:role]
+  end
 
-	bloom :update do
-		term <= update_term { |t| Bud::MaxLattice.new(t.term) }
-		term_voted <= update_max_term_voted { |t| Bud::MaxLattice.new(t.max_term) }
-		role <+- update_role { |r| r if [[:LEADER], [:FOLLOWER], [:CANDIDATE]].include? r }
-	end
+  bloom :update do
+    term <= update_term { |t| Bud::MaxLattice.new(t.term) }
+    term_voted <= update_max_term_voted { |t| Bud::MaxLattice.new(t.max_term) }
+    role <+- update_role { |r| r if [[:LEADER], [:FOLLOWER], [:CANDIDATE]].include? r }
+  end
 
-	bloom :output do
-		current_term <= [[term.reveal]]
-		max_term_voted <= [[term_voted.reveal]]
-		current_role <= role
-		current_members <= members
-	end
+  bloom :output do
+    current_term <= [[term.reveal]]
+    max_term_voted <= [[term_voted.reveal]]
+    current_role <= role
+    current_members <= members
+  end
 end
