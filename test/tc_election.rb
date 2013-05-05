b27b90bf87386ce10bb28219c68b2745bde7234c
Adam Midvidy <amidvidy@gmail.com>
2013-05-05 14:26:40 -0700

    update test for new election interfaces

--- test/tc_election.rb
+++ test/tc_election.rb
@@ -6,20 +6,21 @@ require '../src/election'
 class TestElection < Test::Unit::TestCase
   class Election
     include Bud
-    include LeaderElection
+    include LeaderElectionImpl
 
     bootstrap do
-      add_member <= [
-        [1, '127.0.0.1:54321'],
-        [2, '127.0.0.1:54322'],
-        [3, '127.0.0.1:54323'],
-        [4, '127.0.0.1:54324'],
-        [5, '127.0.0.1:54325']
+      ss.members <= [
+        ['127.0.0.1:54321'],
+        ['127.0.0.1:54322'],
+        ['127.0.0.1:54323'],
+        ['127.0.0.1:54324'],
+        ['127.0.0.1:54325']
       ]
+
     end
   end
 
-  def test_election
+  def test_sanity
     s1 = Election.new(:port => 54321)
     s1.run_bg
     s2 = Election.new(:port => 54322)
@@ -31,7 +32,7 @@ class TestElection < Test::Unit::TestCase
     s5 = Election.new(:port => 54325)
     s5.run_bg
 
-    acks = s1.sync_callback(:start_election, [[1]], :outcome)
+    acks = s1.sync_callback(:start_election, [['127.0.0.1:54321', 1, nil, nil]], :election_outcome)
     assert_equal([[1]], acks)
 
     s1.stop
