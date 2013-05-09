902313ac95ff1f42152b7704f4972919dde22a6a
sankethkatta <sankethkatta@gmail.com>
2013-05-08 23:30:33 -0700

    added a working sequential election test to tc_election

--- test/tc_election.rb
+++ test/tc_election.rb
@@ -42,7 +42,7 @@ class TestElection < Test::Unit::TestCase
     s5.stop(true, true)
   end
 
-  # Two servers start_election for the same term
+  # Two servers start_election for the same term (concurrently)
   def test_multiple_candidates
     s1 = Election.new(:port => 54321)
     s1.run_bg
@@ -61,6 +61,45 @@ class TestElection < Test::Unit::TestCase
       SoftTimeout.timeout(2) do
         acks1 = s1.sync_callback(:start_election, [['127.0.0.1:54321', 1, nil, nil]], :election_outcome)
         acks2 = s2.sync_callback(:start_election, [['127.0.0.1:54322', 1, nil, nil]], :election_outcome)
+      end
+    end
+
+    s1.stop
+    s2.stop
+    s3.stop
+    s4.stop
+    s5.stop(true, true)
+
+  end
+  
+  # Multiple sequential elections
+  def test_sequential_elections
+    puts "test_sequential_elections"
+    s1 = Election.new(:port => 54321)
+    s1.run_bg
+    s2 = Election.new(:port => 54322)
+    s2.run_bg
+    s3 = Election.new(:port => 54323)
+    s3.run_bg
+    s4 = Election.new(:port => 54324)
+    s4.run_bg
+    s5 = Election.new(:port => 54325)
+    s5.run_bg
+
+    # run 2 elections synchronously, first s1 is leader
+    # then s2 is elected afterwards
+
+    s1.sync_do do
+      s1.start_election <+ [['127.0.0.1:54321', 1, nil, nil]]
+      s1.election_outcome do |eo|
+        assert_equal([[1]], eo.term)
+      end
+    end
+
+    s2.sync_do do
+      s2.start_election <+ [['127.0.0.1:54322', 2, nil, nil]]
+      s2.election_outcome do |eo|
+        assert_equal([[2]], eo.term)
       end
     end
 
