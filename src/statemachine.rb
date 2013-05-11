d9d5a394d31cd5ab4d1f6f1d31985060fe665ea5
sankethkatta <sankethkatta@gmail.com>
2013-05-10 23:23:25 -0700

    started implementing the OrderedStateMachine wrapper

--- src/statemachine.rb
+++ src/statemachine.rb
@@ -5,8 +5,8 @@ require 'bud'
 # Implement this protocol with your application-specific state machine
 module StateMachineProto
   state do
-    interface input, :execute_command, [:reqid] => [:command]
-    interface output, :execute_command_resp, [:reqid] => [:command, :new_state]
+    interface input, :execute_command, [:command]
+    interface output, :execute_command_resp, [:new_state]
   end
 end
 
@@ -20,14 +20,44 @@ module OrderedStateMachine
     interface output, :execute_command_resp, [:command_index, :new_state]
 
     table :uncomitted, execute_command.schema
+    table :currently_executing, [] => [:command_index, :command]
     lmax :current_index
+
+    scratch :ready, execute_command.schema
+    scratch :finished, execute_command.schema
   end
 
   bootstrap do
-    current_index <= [[Bud::MaxLattice(0)]]
+    current_index <= Bud::MaxLattice(0)
+  end
+
+  bloom :enqueue_commands do
+    # Buffer all commands into uncomitted
+    uncomitted <= execute_command 
   end
 
   bloom :execute_ordered do
+    # A command is ready if its index is the current index to execute
+    ready <= (uncomitted * current_index).pairs do |u, ci|
+      u if ci.reveal == u.command_index and currently_executing.length == 0
+    end
+
+    # Update the current index
+    current_index <+ ready {|r| Bud::MaxLattice.new(r.index + 1)}
+
+    # Execute the ready commands in the StateMachine
+    sm.execute_command <+ ready  
+
+    # Remove now commited entries from uncomitted
+    uncomitted <- ready
+    currently_executing <+ ready
+  end
+
+  bloom :finish_commands do
+    
+    finished <= sm.execute_command_resp)
+    currently_executing <- finished
 
   end
+
 end
