b906f1cdaf14d6a9a7abd273c217efe33ad67cd2
Adam Midvidy <amidvidy@gmail.com>
2013-05-10 22:00:47 -0700

    add logic to update terms

--- src/log.rb
+++ src/log.rb
@@ -27,7 +27,7 @@ module RaftLog
 
 
   state do
-    channel :append_entries_request_chan, [:@dest, :leader_id, :prev_log_index, :prev_log_term, :entries, :commit_index]
+    channel :append_entries_request_chan, [:@dest, :leader_id, :term, :prev_log_index, :prev_log_term, :entry, :commit_index]
     channel :append_entries_response_chan, [:@dest, :candidate_id, :term, :success]
 
     table :log, [:term, :index] => [:command]
@@ -35,10 +35,14 @@ module RaftLog
 
     lmax :max_index_comitted
 
+    # scratches for master logic
     scratch :prev_index_temp, [:client_id] => [:term, :index]
     scratch :highest_log_entry, log.schema
     scratch :tracked_members, [:client_id]
     scratch :untracked_members, [:client_id]
+
+    # scratches for follower logic
+    scratch :new_entries, [:leader_id, :command, :term, :index, :success]
   end
 
   # When a leader first comes into power it initializes all
@@ -48,8 +52,11 @@ module RaftLog
     highest_log_entry <= log.argmax([:term, :index, :command], :index)
 
     # figure out which members have uninitialized next_index entries
-    tracked_members <= next_indices {|ni| [ni.index]}
-    untracked_members <= current_members.notin(tracked_members, :host => :client_id)
+    tracked_members <= next_indices {|ni| [ni.client_id]}
+    untracked_members <= (tracked_members * current_members).outer(:client_id => :host) do |tm, cm|
+      # someone is untracked if they are not this node and not currently tracket
+      cm if tm == [nil] and cm.host != ip_port
+    end
 
     # leader initializes next_index values
     next_indices <= (current_role * untracked_members * highest_log_entry).combos do |cr, um, hle|
@@ -83,11 +90,27 @@ module RaftLog
       [ni.client_id, entry.term, entry.index] if entry.index == ni.index - 1
     end
 
-    append_entries_request_chan <~ (prev_index_temp * next_indices * log * max_index_comitted)
+    append_entries_request_chan <~ (prev_index_temp * next_indices * log * max_index_comitted * current_term)
       .combos(next_indices.next_index => log.index, prev_index_temp.client_id => next_indices.client_id) \
-    do |prev, ni, entry, mic|
-      [prev.client_id, ip_host, prev.index, prev.term, log.command, mic]
+    do |prev, ni, entry, mic, currterm|
+      [prev.client_id, ip_host, currterm.term, prev.index, prev.term, entry.command, mic]
     end
   end
 
+  bloom :handle_append_entries do
+    # update term if the requestors term is higher than ours, lattice logic handles the details
+    update_term <+ append_entries_request_chan {|req| [req.term]}
+
+    new_entries <~ (log * append_entries_request_chan * current_term)
+      .outer(:index => :pre_log_index, :term => :prev_log_term) do |entry, append_req, currterm|
+      # if we get an appendEntriesRPC from a false leader, ignore it
+      unless append_req.term <= currterm.term
+        if entry != [nil, nil, nil]
+          [append_req.leader_id, append_req.entry, append_req.prev_log_term, append_req.prev_log_index, true]
+        else
+          [append_req.leader_id, append_req.entry, append_req.prev_log_term, append_req.prev_log_index, false]
+        end
+      end
+    end
+  end
 end
