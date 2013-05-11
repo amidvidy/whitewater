80abeeba91fc8467db85d94a475f0ab3376fc536
sankethkatta <sankethkatta@gmail.com>
2013-05-11 03:11:39 -0700

    orderedstatemachine passes all tests

--- test/tc_orderedstatemachine.rb
+++ test/tc_orderedstatemachine.rb
@@ -28,20 +28,38 @@ class TestOrderedStateMachine < Test::Unit::TestCase
   def test_ordered
     @osm.execute_command <+ [[0, ["SET", 10]], 
                              [1, ["DIV", 5]], 
-                             [2, ["SUB", 10]], 
+                             [2, ["SUB", 12]], 
                              [3, ["DIV", -5]], 
                              [4, ["ADD", 7]]]
-    100.times {@osm.tick}
     @osm.tick
     assert_equal [[0, 10]], @osm.delta(:execute_command_resp)
     @osm.tick
-    assert_equal [[0, 10]], @osm.delta(:execute_command_resp)
+    assert_equal [[1, 2]], @osm.delta(:execute_command_resp)
     @osm.tick
-    assert_equal [[0, 10]], @osm.delta(:execute_command_resp)
+    assert_equal [[2, -10]], @osm.delta(:execute_command_resp)
     @osm.tick
-    assert_equal [[0, 10]], @osm.delta(:execute_command_resp)
+    assert_equal [[3, 2]], @osm.delta(:execute_command_resp)
     @osm.tick
-    assert_equal [[0, 10]], @osm.delta(:execute_command_resp)
+    assert_equal [[4, 9]], @osm.delta(:execute_command_resp)
   end
+
+  def test_unordered
+    @osm.execute_command <+ [[0, ["SET", 10]], 
+                             [3, ["DIV", -5]], 
+                             [2, ["SUB", 12]], 
+                             [1, ["DIV", 5]], 
+                             [4, ["ADD", 7]]]
+    @osm.tick
+    assert_equal [[0, 10]], @osm.delta(:execute_command_resp)
+    @osm.tick
+    assert_equal [[1, 2]], @osm.delta(:execute_command_resp)
+    @osm.tick
+    assert_equal [[2, -10]], @osm.delta(:execute_command_resp)
+    @osm.tick
+    assert_equal [[3, 2]], @osm.delta(:execute_command_resp)
+    @osm.tick
+    assert_equal [[4, 9]], @osm.delta(:execute_command_resp)
+  end
+
 
 end
